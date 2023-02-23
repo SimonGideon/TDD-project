dcfe44889fc8d88d877a7cab2bdd1b668f99776d
SimonGideon <simongideon918@gmail.com>
2023-02-24 02:28:09 +0300

    Refactor file

--- spec/solved_spec.rb
+++ spec/solved_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../solved'
 
 describe Solved do
+    describe '#factorial' do
+        it 'returns 1 for 0' do
+            expect(subject.factorial(0)).to eq(1)
+        end
 
+        it 'returns 1 for 1' do
+            expect(subject.factorial(1)).to eq(1)
+        end
+
+        it 'returns factorial >1 number' do
+            expect(subject.factorial(5)).to eq(120)
+        end
+    end
 end
