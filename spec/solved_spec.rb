58596b7705ca54384e3f01f050b40a5f5efe252e
SimonGideon <simongideon918@gmail.com>
2023-02-24 02:40:18 +0300

    Test fizbuzz

--- spec/solved_spec.rb
+++ spec/solved_spec.rb
@@ -24,4 +24,22 @@ describe Solved do
             expect { subject.reverse }.to raise_error(ArgumentError)
         end
     end
+
+    describe '#fizzbuzz' do
+        it 'returns fizz for 3' do
+            expect(subject.fizzbuzz(3)).to eq('fizz')
+        end
+
+        it 'returns buzz for 5' do
+            expect(subject.fizzbuzz(5)).to eq('buzz')
+        end
+
+        it 'returns fizzbuzz for 15' do
+            expect(subject.fizzbuzz(15)).to eq('fizzbuzz')
+        end
+
+        it 'returns number for 1' do
+            expect(subject.fizzbuzz(1)).to eq(1)
+        end
+    end
 end
