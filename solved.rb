d8faab1c155ad76b341449018e38f975c0f51b9d
SimonGideon <simongideon918@gmail.com>
2023-02-24 02:41:07 +0300

    Fizbuzz method

--- solved.rb
+++ solved.rb
@@ -10,4 +10,16 @@ class Solved
     def reverse(str)
         str.reverse
     end
+
+    def fizzbuzz(n)
+        if n % 15 == 0
+            'fizzbuzz'
+        elsif n % 3 == 0
+            'fizz'
+        elsif n % 5 == 0
+            'buzz'
+        else
+            n
+        end
+    end
 end
