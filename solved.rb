18be2c17a208cb910065a31cf0abaceece54c4d4
SimonGideon <simongideon918@gmail.com>
2023-02-24 02:20:02 +0300

    factorial testing

--- solved.rb
+++ solved.rb
@@ -1,3 +1,16 @@
 class Solved
-    
+    describe '#factorial' do
+        solve = Solved.new()
+        it 'returns 1 for 0' do
+            expect(solve.factorial(0)).to eq(1)
+        end
+
+        it 'returns 1 for 1' do
+            expect(solve.factorial(1)).to eq(1)
+        end
+
+        it 'returns factorial >1 number' do
+            expect(solve.factorial(5)).to eq(120)
+        end
+    end
 end
